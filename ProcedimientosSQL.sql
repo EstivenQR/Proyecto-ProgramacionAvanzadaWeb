197eb2804f925884c5121e16a9c851a493918f1b
403leo <leonardomadsanchez@gmail.com>
2025-03-24 11:27:32 -0600

    Commit Procedimientos SQL
    Se hizo el commit para procedimeintos que se utilizaran en un futuro para el logeo.

---
+++ ProcedimientosSQL.sql
@@ -0,0 +1,31 @@
+USE BaseDatos_ProyectoLibreria
+GO 
+
+CREATE OR ALTER PROCEDURE sp_Login(@User NVARCHAR(MAX), @Contraseña NVARCHAR(MAX), @Exitos BIT OUTPUT)
+AS 
+BEGIN
+	IF( SELECT COUNT(*) FROM Usuario WHERE Username = @User AND Password = @Contraseña) = 1
+	BEGIN
+		SET @Exitos =1 
+	END
+	ELSE
+	BEGIN
+		SET @Exitos =0
+	END
+	SELECT @Exitos
+END
+
+GO
+DECLARE @F bit
+EXEC sp_Login 'leo3047','123', @F OUTPUT
+GO
+
+CREATE OR alter PROCEDURE sp_ObtenerUsuario(@User NVARCHAR(MAX), @Contraseña NVARCHAR(MAX))
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	SELECT * FROM Usuario WHERE Username = @User AND Password = @Contraseña
+END
+
+GO
+EXEC sp_ObtenerUsuario 'Zoe123','123'
