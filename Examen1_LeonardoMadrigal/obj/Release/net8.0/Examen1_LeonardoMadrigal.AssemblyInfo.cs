8eea22914f9eba2a2baba2e7c45606773e5f4b89
JoshuaMeji <156728872+JoshuaMeji@users.noreply.github.com>
2025-03-24 18:55:48 -0600

    Inicio Prestamo

--- Examen1_LeonardoMadrigal/obj/Release/net8.0/Examen1_LeonardoMadrigal.AssemblyInfo.cs
+++ Examen1_LeonardoMadrigal/obj/Release/net8.0/Examen1_LeonardoMadrigal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen1_LeonardoMadrigal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2c94ca59ea1ed4a72bf31510409cfc7238e16c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fb1ed6b26ffc1ef96ea36baa12f4c998f66273")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen1_LeonardoMadrigal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen1_LeonardoMadrigal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
